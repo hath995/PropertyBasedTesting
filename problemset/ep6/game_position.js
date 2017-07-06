b9b01202b33e2a1b531c4e291aa5d4d3c064611c
hath995 <hathlesstroubles@gmail.com>
2017-07-05 19:45:32 -0700

    added game_position description

--- problemset/ep6/game_position.js
+++ problemset/ep6/game_position.js
@@ -1,5 +1,8 @@
 /*
+ * In a particular board game, players have to try to advance through a sequence of positions. 
+ * Each position has a nonnegative integer associated with it, representing the maximum you can advance from that position in one move. For example, let A = [3,3,1,0,2,0,1] represent the board game i.e. the i-th entry in A is the maximum we can advance from i. If A = [3,2,0,0,2,0,1] it is not possible to advance past position 3
  *
+ * Write a function which takes an array of n integers, where A[i] denotes the maximum you can advance from the index i, and returns whether is is possible (true, false) to advance to the last index starting from the beginning of the array
  */
 function winnable(game) {
 }
