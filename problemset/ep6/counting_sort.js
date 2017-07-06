d127b871f5a56a73a301e19086daa6dd0a945a20
hath995 <hathlesstroubles@gmail.com>
2017-07-05 19:51:15 -0700

    fix description

--- problemset/ep6/counting_sort.js
+++ problemset/ep6/counting_sort.js
@@ -1,8 +1,8 @@
 /*
  * Write a function that takes an array A and an index 
  * and rearranges the elements such that all elements less than A[i] 
- * appearfirst, followed by elements equal to A[i], followed by elements greater than A[i],
- * Your algorithm should have 0(1) space complexity and 0(1.41)time complexity.
+ * appear first, followed by elements equal to A[i], followed by elements greater than A[i],
+ * Your algorithm should have O(1) space complexity and O(A) time complexity.
  */
 function counting_sort(items, index) {
 }
